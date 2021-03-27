4cdd77346769ac361fce92700f109607f4b51d3b
Tyler <tyler_venetsanos@msn.com>
2021-03-27 13:39:06 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+const cards = ["Ada", "Brendan", "Ali"];
+
+function writeCards(cards) {
+  for (let i = 0; i < cards.length; i++) {
+    console.log(`Thank you, ${cards[i]}, for the wonderful birthday gift!`);
+  }
+
+  return cards;
+}
+
+writeCards(cards); 
+
+let countdown = 10;
+while (countdown > 0) {
+  console.log(countdown--);
+} 
